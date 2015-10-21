6c58208b2f73a13437b421a7a372682412af5f10
sls-kwan <shu.lun.shannon.kwan@gmail.com>
2015-10-21 15:29:20 +1100

    Allow user to submit own csv file and writes csv to download folder

--- app/scripts/readCSV.js
+++ app/scripts/readCSV.js
@@ -1,13 +1,45 @@
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "scripts/data.csv",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
+
+var chooseFileButton = document.querySelector('#choose_file');
+var textarea = document.querySelector('textarea');
+var data = null;
+
+function errorHandler(e) {
+  console.error(e);
+}
+function readAsText(inFile, callback) {
+  inFile.file(function(file) {
+    var reader = new FileReader();
+
+    reader.onerror = errorHandler;
+    reader.onload = function(e) {
+      callback(e.target.result);
+    };
+
+    reader.readAsText(file);
+  });
+}
+function loadFileEntry(_chosenEntry) {
+  chosenEntry = _chosenEntry;
+  chosenEntry.file(function(file) {
+    readAsText(chosenEntry, function(result) {
+			data = $.csv.toObjects(result);
+			textarea.value = data[0].Type;
+    });
+  });
+}
+
+chooseFileButton.addEventListener('click', function(e) {
+  var accepts = [{
+    mimeTypes: ['text/*'],
+    extensions: ['csv']
+  }];
+  chrome.fileSystem.chooseEntry({type: 'openFile', accepts: accepts}, function(theFile) {
+    if (!theFile) {
+      return;
+    }
+    // use local storage to retain access to this file
+    chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(theFile)});
+    loadFileEntry(theFile);
+  });
 });
 
-function processData(allText) {
-	var data = $.csv.toObjects(allText);
-	console.log(data[0].test);
-}
--- app/scripts/writeCSV.js
+++ app/scripts/writeCSV.js
@@ -1,9 +1,11 @@
-var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
+var data = [["Type", "Day", "Start", "End"], ["name2", "city2", "more info"]];
+var testAppend = ["test", "Test", "01100", "0010000"]
+data.push(testAppend);
 var csvContent = "data:text/csv;charset=utf-8,";
 
-data.forEach(function(infoArray, index){
+data.forEach(function(inputArray, index){
 
-   dataString = infoArray.join(",");
+   dataString = inputArray.join(",");
    csvContent += index < data.length ? dataString+ "\n" : dataString;
 
 }); 
@@ -11,6 +13,7 @@ data.forEach(function(infoArray, index){
 var encodedUri = encodeURI(csvContent);
 var link = document.createElement("a");
 link.setAttribute("href", encodedUri);
+
 link.setAttribute("download", "my_data.csv");
 
-link.click()
+link.click();
