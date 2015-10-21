82545fe1e57d63a9684c2119e3fc5c3d4579254d
sls-kwan <shu.lun.shannon.kwan@gmail.com>
2015-10-21 16:18:01 +1100

    More explanation in the code ie. comments and in the README

--- app/scripts/readCSV.js
+++ app/scripts/readCSV.js
@@ -1,5 +1,6 @@
-
+//Captures which file you selected
 var chooseFileButton = document.querySelector('#choose_file');
+//Display the Type column in the CSV file
 var textarea = document.querySelector('textarea');
 var data = null;
 
@@ -22,13 +23,18 @@ function loadFileEntry(_chosenEntry) {
   chosenEntry = _chosenEntry;
   chosenEntry.file(function(file) {
     readAsText(chosenEntry, function(result) {
+			//This uses the CSV library jquery-csv
 			data = $.csv.toObjects(result);
+			//Displays the First element in the Type column in the text area
 			textarea.value = data[0].Type;
     });
   });
 }
 
 chooseFileButton.addEventListener('click', function(e) {
+	//When the user clicks on the Choose File button 
+	//It excecutes this code, only allowing the user 
+	//to select csv files
   var accepts = [{
     mimeTypes: ['text/*'],
     extensions: ['csv']
--- app/scripts/writeCSV.js
+++ app/scripts/writeCSV.js
@@ -1,4 +1,8 @@
+//This is the array that we are going to make into CSV
+//Each line is one array element as such the first line of the CSV file 
+//is: Type, Day, Start, End
 var data = [["Type", "Day", "Start", "End"], ["name2", "city2", "more info"]];
+
 var testAppend = ["test", "Test", "01100", "0010000"]
 data.push(testAppend);
 var csvContent = "data:text/csv;charset=utf-8,";
@@ -15,5 +19,5 @@ var link = document.createElement("a");
 link.setAttribute("href", encodedUri);
 
 link.setAttribute("download", "my_data.csv");
-
+//Downloads the csv file into download folder
 link.click();
